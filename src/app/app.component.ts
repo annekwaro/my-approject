6133edbeee6c27dc128194cb8026639313291c69
annekwaro <annekwaro547@gmail.com>
2023-12-18 17:09:09 +0100

    updated style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-approject';
+  pageTitle: string = 'Angular HTTP Client';
+  users = new Array<any>();
+  constructor(private userService: UserService) {}
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((data) => {
+      this.users = data;
+    });
+  }
 }
